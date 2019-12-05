bbf7dd6632266553490885e444fb4a4ce4b7b12a
Kristian Garza <kj.garza@gmail.com>
2019-12-05 10:49:19 +0100

    linting

--- src/components/MediumWidget.vue
+++ src/components/MediumWidget.vue
@@ -1,47 +1,58 @@
 <template>
   <div class="metrics">
-
-
-    <div class="wrapper" v-bind:title=tooltip>
+    <div
+      class="wrapper"
+      :title="tooltip"
+    >
       <div>
-        <!-- <img src="../assets/medium_widget.png" v-bind:alt="this.alt"> -->
-        <mediumBadge class="medium-badge" v-bind:alt="this.alt" /> 
+        <mediumBadge
+          class="medium-badge"
+          :alt="this.alt"
+        />
       </div>
 
       <div class="metrics">
-        <a v-if="views > 0" class="usage-counts usage-views" v-bind:href="link+'#views-tab'">
-          <viewIcon class="icon-metrics" /> 
-          {{views}} Views<br/>
+        <a
+          v-if="views > 0"
+          class="usage-counts usage-views"
+          :href="link + '#views-tab'"
+        >
+          <viewIcon class="icon-metrics" />
+          {{ views }} Views<br>
         </a>
-        
-        <a v-if="downloads > 0" class="usage-counts usage-downloads" v-bind:href="link+'#downloads-tab'">
-          <downloadIcon class="icon-metrics" />
-          {{downloads}} Downloads<br/>
-        </a>
-        
-        <a v-if="citations > 0" class="usage-counts usage-downloads" v-bind:href="link+'#citations-tab'">
-          <citationIcon class="icon-metrics" />  
 
-          {{citations}} Citations<br/>
+        <a
+          v-if="downloads > 0"
+          class="usage-counts usage-downloads"
+          :href="link + '#downloads-tab'"
+        >
+          <downloadIcon class="icon-metrics" />
+          {{ downloads }} Downloads<br>
         </a>
-        
+
+        <a
+          v-if="citations > 0"
+          class="usage-counts usage-downloads"
+          :href="link + '#citations-tab'"
+        >
+          <citationIcon class="icon-metrics" />
+
+          {{ citations }} Citations<br>
+        </a>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-
-import BaseWidget from '@/models/BaseWidget.vue'
+// import { VBadge, VIcon } from 'vuetify/lib';
+import BaseWidget from '@/models/BaseWidget.vue';
 
 export default {
   name: 'MediumWidget',
-  extends: BaseWidget
-}
+  extends: BaseWidget,
+};
 </script>
-
-
 
 <style scoped>
 .wrapper {
@@ -60,8 +71,7 @@ export default {
   display: block;
   margin: auto;
   /* margin-right: 3px; */
-  
+
   /* display:inline-block; */
 }
-
 </style>
