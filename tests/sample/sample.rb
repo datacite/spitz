078f46dd72d1da7f31ab1465d645aed6670f2898
Kristian Garza <kj.garza@gmail.com>
2019-12-15 16:57:09 +0100

    sample testing for browsers
    https://github.com/datacite/spitz/issues/4

--- tests/sample/sample.rb
+++ tests/sample/sample.rb
@@ -1,47 +1,61 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'selenium-webdriver'
-# require 'browserstack/local'
 
-#stop the Local instance
-# bs_local.stop
+browsers = [
+  {
+    browser: 'Firefox',
+    browser_version: '71.0',
+    name: 'Test with Firefox',
+    os: 'Windows',
+    os_version: '10'
+  },
+  {
+    browser: 'Chrome',
+    browser_version: '76',
+    name: 'Test with Chrome',
+    os: 'Windows',
+    os_version: '10'
+  },
+  {
+    browser: 'Safari',
+    browser_version: '13',
+    name: 'Test with safari'
+  },
+  {
+    browser: 'Edge',
+    browser_version: '18.0',
+    name: 'Test with Edge',
+    os: 'Windows',
+    os_version: '10'
 
-# #creates an instance of Local
-# bs_local = BrowserStack::Local.new
+  }
+]
 
-# #replace <browserstack-accesskey> with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
-# bs_local_args = { "key" => "kBoF121yeasHAQQwU93c" }
+browsers.each do |browser| 
+  # Input capabilities
+  caps = Selenium::WebDriver::Remote::Capabilities.new
+  caps['browser'] = browser.fetch(:browser, 'Firefox')
+  caps['browser_version'] = browser.fetch(:browser_version, '71.0')
+  caps['os'] =  browser.fetch(:os, 'OS X')
+  caps['os_version'] = browser.fetch(:os_version, 'Catalina')
+  caps['resolution'] = '1024x768'
+  caps['name'] = browser.fetch(:name, 'test')
+  caps[:logging_prefs] = {:browser => "ALL"}
+  caps['console'] = "errors"
 
-# #starts the Local instance with the required arguments
-# bs_local.start(bs_local_args)
+  driver = Selenium::WebDriver.for(:remote,
+                                   url: 'http://kristiangarza1:kBoF121yeasHAQQwU93c@hub-cloud.browserstack.com/wd/hub',
+                                   desired_capabilities: caps)
+  driver.navigate.to 'https://support.datacite.org/docs/displaying-usage-and-citations-in-your-repository'
+ 
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
+  sleep 10
 
+  driver.save_screenshot("./tests/sample/#{caps['browser']}_#{caps['browser_version']}_sample_test.png")
 
-# Input capabilities
-caps = Selenium::WebDriver::Remote::Capabilities.new
-caps['browser'] = 'Firefox'
-caps['browser_version'] = '71.0'
-caps['os'] = 'OS X'
-caps['os_version'] = 'Catalina'
-caps['resolution'] = '1024x768'
-caps['name'] = 'Bstack-[Ruby] Sample Test'
-
-
-driver = Selenium::WebDriver.for(:remote,
-  :url => "http://kristiangarza1:kBoF121yeasHAQQwU93c@hub-cloud.browserstack.com/wd/hub",
-  :desired_capabilities => caps)
-driver.navigate.to "https://support.datacite.org/docs/displaying-usage-and-citations-in-your-repository"
-el = driver.find_element(:xpath, '//data-metrics-badge')
-driver.action.move_to(el).perform
-
-# element.send_keys "BrowserStack"
-# element.submit
-driver.save_screenshot("sample.png")
-
-# puts driver.title
-driver.quit
-
-# #check if BrowserStack local instance is running
-# puts bs_local.isRunning
-
-# #stop the Local instance
-# bs_local.stop
+  puts "Completed Sample test for #{caps['browser']}_#{caps['browser_version']}"
+  driver.quit
+end
